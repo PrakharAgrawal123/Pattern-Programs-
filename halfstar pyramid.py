82c6497ba01e649502f2c3703bf1714645624afd
Prakhar Agrawal <agrawalprakhar931@gmail.com>
2025-05-07 18:43:29 +0530

    Add files via upload

---
+++ halfstar pyramid.py
@@ -0,0 +1,8 @@
+#Program to print inverted half pyramid using * 
+
+rows = int(input("Enter number of rows: "))
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("* ", end=" ")
+    
+    print()
